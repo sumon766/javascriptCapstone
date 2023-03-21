087cdc571724eabb39c114ba55258faf82716620
Sumon <sumon766@gmail.com>
2023-03-21 18:43:39 +0600

    Webpack setup done

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, "src", "index.html")
+        })
+    ],
+
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+          }
+        ]
+    },
+};
