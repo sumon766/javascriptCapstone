0636bd88c4005b7e7d54246fbe56df80dfef0fb8
MishDM <mish@g.com>
2023-03-22 09:22:56 -0700

    Fix linter issues

--- src/modules/apiCall.js
+++ src/modules/apiCall.js
@@ -1,150 +1,150 @@
 const address = 'https://api.tvmaze.com/show';
 const getMovies = async () => {
-    const list = await fetch(address);
-    const movies = await list.json();
-    return movies;
-}
+  const list = await fetch(address);
+  const movies = await list.json();
+  return movies;
+};
 
 const showMovies = async () => {
-    const received = await getMovies();
-    console.log(received);
-    const programs = document.getElementById('programs');
+  const received = await getMovies();
+  console.log(received);
+  const programs = document.getElementById('programs');
 
-    for (let i = 0; i < received.length; i += 1) {
-        if(received[i].id < 7) {
-            let program = document.createElement('div');
-            program.className = 'program';
+  for (let i = 0; i < received.length; i += 1) {
+    if (received[i].id < 7) {
+      const program = document.createElement('div');
+      program.className = 'program';
 
-            let image = document.createElement('img');
-            image.src = received[i].image.medium;
-            program.appendChild(image);
+      const image = document.createElement('img');
+      image.src = received[i].image.medium;
+      program.appendChild(image);
 
-            let title = document.createElement('h2');
-            title.innerText = received[i].name;
-            program.appendChild(title);
+      const title = document.createElement('h2');
+      title.innerText = received[i].name;
+      program.appendChild(title);
 
-            let likeCount = document.createElement('div');
-            likeCount.className = 'like-count';
-            program.appendChild(likeCount);
+      const likeCount = document.createElement('div');
+      likeCount.className = 'like-count';
+      program.appendChild(likeCount);
 
-            let likeButton = document.createElement('i');
-            likeButton.className = 'fa fa-heart-o like';
-            likeCount.appendChild(likeButton);
+      const likeButton = document.createElement('i');
+      likeButton.className = 'fa fa-heart-o like';
+      likeCount.appendChild(likeButton);
 
-            let likeNumber = document.createElement('p');
-            likeNumber.innerText = '5 likes';
-            likeCount.appendChild(likeNumber);
+      const likeNumber = document.createElement('p');
+      likeNumber.innerText = '5 likes';
+      likeCount.appendChild(likeNumber);
 
-            let commentButton = document.createElement('button');
-            commentButton.id = 'comment-button-' + received[i].id;
-            commentButton.type = 'button';
-            commentButton.innerText = 'Comment';
-            program.appendChild(commentButton);
+      const commentButton = document.createElement('button');
+      commentButton.id = `comment-button-${received[i].id}`;
+      commentButton.type = 'button';
+      commentButton.innerText = 'Comment';
+      program.appendChild(commentButton);
 
-            let moviesModal = document.createElement('div');
-            moviesModal.className = 'modal';
-            moviesModal.id = 'comment-modal-' + received[i].id;
-            program.appendChild(moviesModal);
+      const moviesModal = document.createElement('div');
+      moviesModal.className = 'modal';
+      moviesModal.id = `comment-modal-${received[i].id}`;
+      program.appendChild(moviesModal);
 
-            let modalContent = document.createElement('div');
-            modalContent.className = 'modal-content';
-            moviesModal.appendChild(modalContent);
+      const modalContent = document.createElement('div');
+      modalContent.className = 'modal-content';
+      moviesModal.appendChild(modalContent);
 
-            let close = document.createElement('span');
-            close.className = `close${received[i].id}`;
-            close.id = 'close';
-            close.innerHTML = '&times;';
-            modalContent.appendChild(close);
+      const close = document.createElement('span');
+      close.className = `close${received[i].id}`;
+      close.id = 'close';
+      close.innerHTML = '&times;';
+      modalContent.appendChild(close);
 
-            let modalImageTitle = document.createElement('div');
-            modalImageTitle.className = 'modal-image-title';
-            modalContent.appendChild(modalImageTitle);
+      const modalImageTitle = document.createElement('div');
+      modalImageTitle.className = 'modal-image-title';
+      modalContent.appendChild(modalImageTitle);
 
-            let modalImage = document.createElement('img');
-            modalImage.src = received[i].image.medium;
-            modalImageTitle.appendChild(modalImage);
+      const modalImage = document.createElement('img');
+      modalImage.src = received[i].image.medium;
+      modalImageTitle.appendChild(modalImage);
 
-            let modalDescription = document.createElement('div');
-            modalDescription.className = 'modal-description';
-            modalImageTitle.appendChild(modalDescription);
+      const modalDescription = document.createElement('div');
+      modalDescription.className = 'modal-description';
+      modalImageTitle.appendChild(modalDescription);
 
-            let modalTitle = document.createElement('h2');
-            modalTitle.innerText = received[i].name;
-            modalDescription.appendChild(modalTitle);
+      const modalTitle = document.createElement('h2');
+      modalTitle.innerText = received[i].name;
+      modalDescription.appendChild(modalTitle);
 
-            let storyLine = document.createElement('p');
-            storyLine.innerHTML = received[i].summary;
-            modalDescription.appendChild(storyLine);
+      const storyLine = document.createElement('p');
+      storyLine.innerHTML = received[i].summary;
+      modalDescription.appendChild(storyLine);
 
-            // Manual Comments Here
-            let comments = document.createElement('h2');
-            comments.innerText = 'Comments (2)';
-            modalDescription.appendChild(comments);
+      // Manual Comments Here
+      const comments = document.createElement('h2');
+      comments.innerText = 'Comments (2)';
+      modalDescription.appendChild(comments);
 
-            let comment1 = document.createElement('p');
-            comment1.innerHTML = '<span>22/03/2023 </span>Lorem ipsum dolor sit amet consectetur adipisicing elit.';
-            modalDescription.appendChild(comment1);
+      const comment1 = document.createElement('p');
+      comment1.innerHTML = '<span>22/03/2023 </span>Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+      modalDescription.appendChild(comment1);
 
-            let comment2 = document.createElement('p');
-            comment2.innerHTML = '<span>20/03/2023 </span>Lorem ipsum dolor sit amet consectetur adipisicing elit.';
-            modalDescription.appendChild(comment2);
-            // Manual Comments Here
+      const comment2 = document.createElement('p');
+      comment2.innerHTML = '<span>20/03/2023 </span>Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+      modalDescription.appendChild(comment2);
+      // Manual Comments Here
 
-            // Comment Form Starts Here
-            let form = document.createElement('form');
-            form.id = 'comment-form';
-            modalDescription.appendChild(form);
+      // Comment Form Starts Here
+      const form = document.createElement('form');
+      form.id = 'comment-form';
+      modalDescription.appendChild(form);
 
-            let fieldset = document.createElement('fieldset');
-            form.appendChild(fieldset);
+      const fieldset = document.createElement('fieldset');
+      form.appendChild(fieldset);
 
-            let legend = document.createElement('legend');
-            legend.innerText = 'Add Comment';
-            fieldset.appendChild(legend);
+      const legend = document.createElement('legend');
+      legend.innerText = 'Add Comment';
+      fieldset.appendChild(legend);
 
-            let name = document.createElement('input');
-            name.type = 'text';
-            name.id = 'name';
-            name.name = 'name';
-            name.placeholder = 'Your Name';
-            name.required = 'required';
-            fieldset.appendChild(name);
+      const name = document.createElement('input');
+      name.type = 'text';
+      name.id = 'name';
+      name.name = 'name';
+      name.placeholder = 'Your Name';
+      name.required = 'required';
+      fieldset.appendChild(name);
 
-            let commentText = document.createElement('textarea');
-            commentText.name = 'comment';
-            commentText.id = 'comment';
-            commentText.placeholder = 'Your Comment';
-            fieldset.appendChild(commentText);
+      const commentText = document.createElement('textarea');
+      commentText.name = 'comment';
+      commentText.id = 'comment';
+      commentText.placeholder = 'Your Comment';
+      fieldset.appendChild(commentText);
 
-            let submit = document.createElement('input');
-            submit.type = 'submit';
-            submit.name = 'submit';
-            submit.value = 'Submit';
-            submit.id = 'submit';
-            fieldset.appendChild(submit);
-            // Comment Form Starts Here
+      const submit = document.createElement('input');
+      submit.type = 'submit';
+      submit.name = 'submit';
+      submit.value = 'Submit';
+      submit.id = 'submit';
+      fieldset.appendChild(submit);
+      // Comment Form Starts Here
 
-            programs.appendChild(program);
+      programs.appendChild(program);
 
-            let modal = document.getElementById(`comment-modal-${received[i].id}`);
-            let btn = document.getElementById(`comment-button-${received[i].id}`);
-            let span = document.getElementsByClassName(`close${received[i].id}`)[0];
-        
-            btn.onclick = function() {
-                modal.style.display = "block";
-            }
-        
-            span.onclick = function() {
-                modal.style.display = "none";
-            }
-        
-            window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
+      const modal = document.getElementById(`comment-modal-${received[i].id}`);
+      const btn = document.getElementById(`comment-button-${received[i].id}`);
+      const span = document.getElementsByClassName(`close${received[i].id}`)[0];
+
+      btn.onclick = function () {
+        modal.style.display = 'block';
+      };
+
+      span.onclick = function () {
+        modal.style.display = 'none';
+      };
+
+      window.onclick = function (event) {
+        if (event.target === modal) {
+          modal.style.display = 'none';
         }
+      };
     }
-}
+  }
+};
 
 export default showMovies;
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,21 +1,21 @@
 const modal = () => {
-    var modal = document.getElementById("comment-modal");
-    var btn = document.getElementById("comment-button");
-    var span = document.getElementsByClassName("close")[0];
+  const modal = document.getElementById('comment-modal');
+  const btn = document.getElementById('comment-button');
+  const span = document.getElementsByClassName('close')[0];
 
-    btn.onclick = function() {
-    modal.style.display = "block";
-    }
+  btn.onclick = function () {
+    modal.style.display = 'block';
+  };
 
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
+  span.onclick = function () {
+    modal.style.display = 'none';
+  };
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
     }
-}
+  };
+};
 
 export default modal;
