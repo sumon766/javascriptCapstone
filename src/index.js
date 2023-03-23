7ce0f40a734ebd59b9bec3b288f199b863396fa7
MishDM <mish@g.com>
2023-03-23 07:26:58 -0700

    Add  movie counter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import './images/movie.jpg';
 import showMovies from './modules/apiCall';
+import movieCounter from './modules/movieCounter';
 
 window.onload = showMovies();
+window.onload = movieCounter()
